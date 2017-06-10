7b9f9a083208bd1fcf553def1d5de590d290990a
codexmrashid <mrashid@codexin.com>
2017-06-10 13:10:36 +0600

    some modifyed

--- js/main.js
+++ js/main.js
@@ -17,6 +17,7 @@
             $(".shown").addClass( "show" );
 
      });
+    
      $(".closs").click(function(e) {
             e.preventDefault();
 
@@ -28,42 +29,73 @@
      });
 
 
-    $( ".open_video" ).click(function(e) {
-        e.preventDefault();
-       
-        if (!$(this).hasClass('disabled')) {
 
-            var htmlString = $( ".append" ).html();
-            $(this).parents( ".one_row" ).append( htmlString );
-
-            // $('.open_video').click(function(){return false;});
-            $('.open_video').addClass("disabled");
-
-            $('html,body').animate({
-                scrollTop: $(".append_video").offset().top},
-                'slow');
-        
-        }
-        // else{
-        // $(".closs2").click(function(e) click korate hbe 
-        // }
-
-        close_append();
-    });
+    // $( ".open_video" ).click(function(e) {
+    //     e.preventDefault();
+    //     var htmlString = $( ".append" ).html();
+    //     $( '.append_video' ).html( htmlString );
+    //     $(".append_video").removeClass( "hide" );
+    //     $(".append_video").addClass( "show" );
+    //     close_append();
+    // });
 
 
     // $(".open_video").click(function() {
-        
+    //     $('html,body').animate({
+    //         scrollTop: $(".append_video").offset().top},
+    //         'slow');
     // });
 
-    function close_append(){
-        $(".closs2").click(function(e) {
-            e.preventDefault();
-            $(this).parents( ".append_video" ).remove();
-            $('.open_video').removeClass("disabled");
+    // function close_append(){
+    //     $(".closs2").click(function(e) {
+    //         e.preventDefault();
+
+    //         $(".append_video").removeClass( "show" );
+    //         $(".append_video").addClass( "hide" );
+    //     });
+    // }
+
+
+
+
+
+
+    // $( ".open_video" ).click(function(e) {
+    //     e.preventDefault();
+       
+    //     if (!$(this).hasClass('disabled')) {
+
+    //         var htmlString = $( ".append" ).html();
+    //         $(this).parents( ".one_row" ).find('.append_video').html( htmlString );
+
+    //         // $('.open_video').click(function(){return false;});
+    //         $('.open_video').addClass("disabled");
+
+    //         $('html,body').animate({
+    //             scrollTop: $(".append_video").offset().top},
+    //             'slow');
+        
+    //     }
+    //     // else{
+    //     // $(".closs2").click(function(e) click korate hbe 
+    //     // }
+
+    //     close_append();
+    // });
+
+
+    // // $(".open_video").click(function() {
+        
+    // // });
+
+    // function close_append(){
+    //     $(".closs2").click(function(e) {
+    //         e.preventDefault();
+    //         $(this).parents( ".col" ).addClass('hide');
+    //         $('.open_video').removeClass("disabled");
             
-        });
-    }
+    //     });
+    // }
 
 
 	$(".slick-slider").slick({
